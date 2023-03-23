5646a2c6b54405b2e7736edad97dbe583715c415
johnavarro1 <98787399+johnavarro1@users.noreply.github.com>
2023-03-23 00:23:33 -0700

    Block.py functions

--- Block.py
+++ Block.py
@@ -1,13 +1,19 @@
 # TODO Johnny
-import os
 import datetime
-import time
+import struct
 
 def getUnixTime():
    
     presentDate = datetime.datetime.now()
-    unix_timestamp = datetime.datetime.timestamp(presentDate)*1000 
-    return unix_timestamp
+    # unix_timestamp = datetime.datetime.timestamp(presentDate)*1000
+    isoTime = presentDate.isoformat()
+    return  isoTime
+
+def getIso8601Timestamp(double_timestamp):
+    unix_timestamp = struct.unpack('d', double_timestamp)[0] / 1000
+    dt = datetime.datetime.fromtimestamp(unix_timestamp)
+    iso8601_timestamp = dt.strftime('%Y-%m-%dT%H:%M:%S.%f')
+    return iso8601_timestamp
 
 class Block:
     def __init__(self):
@@ -18,6 +24,7 @@ class Block:
         self.State = None
         self.DataLen = 0
         self.Data = None
+
         
     def getPreviousHash(self):
         return self.PreviousHash
@@ -60,32 +67,82 @@ class Block:
 
     def setData(self, data):
         self.Data = data
+    
+    def getDoubleTimestamp(self):
+        dt = datetime.datetime.strptime(self.getTimestamp(), '%Y-%m-%dT%H:%M:%S.%f')
+        unix_timestamp = datetime.datetime.timestamp(dt) * 1000
+        double_timestamp = struct.pack('d', unix_timestamp)
+        return double_timestamp
+
+    #need to work on formatting this
+    def blockToBytes(self):
+
+        block = bytes((str(self.getPreviousHash())),'utf-8')
         
-class static_block:
-    #padding info (length in bytes) for state
-    bytelen12 = 12
-    #actual state assignment
-    stateVar = b'\2'
-    
-    PreviousHash = "26 19 13 b7 1a 13 30 6d 63 d6 56 12 87 5e ad 13 ed 28 1b 2f 70 f9 c7 13 76 12 47 f4 7a 9f f7 ee "
-    
-    #function defined at the top, need to change to make 8 byte long float 
-    Timestamp = getUnixTime()
-    CID = 9269517611667619
-    EID = 2179
-    
-    #force padding of "a"s to the actual state assignment
-    State = stateVar.rjust(bytelen12, b'\a')
-    #length of following Data
-    DataLen = 100
-    #Data at length of 100
-    Data = "uwF6MJKlWZ6G1JQQuqXRVHAwYIusJQBhhL7KFtrnxnN9xTwhESWbAi6MI09OfyGYUFbp8vUMEJcwCIJInReYggGt9HsS3QzjDc1h"
+        # print("***"+ self.getTimestamp())
+        # y = self.getDoubleTimestamp()
+        # print("***"+str(y))
+        # print("***"+str(getIso8601Timestamp(y)))
         
-print("Static Block contents: ")
-print("Previous hash: " + static_block.PreviousHash)
-print("Timestamp: " + str(static_block.Timestamp))
-print("CID: " + str(static_block.CID))
-print("EID: " + str(static_block.EID))
-print("State: " + str(static_block.State))
-print("Data Length: " + str(static_block.DataLen))
-print("Data: " + static_block.Data)
+        block += bytes(str(self.getDoubleTimestamp()),'utf-8')
+        block += bytes(hex(self.getCID())[2:],'utf-8')
+        block += bytes(hex(self.getEID())[2:],'utf-8')
+        block += bytes(hex(self.getState())[2:],'utf-8')
+        block += bytes(hex(self.getDataLength())[2:],'utf-8')
+        s = self.getData().encode('utf-8')
+        block += bytes(s.hex(),'utf-8')
+        print(block)
+        # out = open(str(self.getTimestamp())+".raw", "wb")
+        # out.write(block)
+        
+        
+    def printBlock(self):
+        print("Static Block contents: --------------------------------------")
+        print("Previous hash: " + self.PreviousHash)
+        print("Timestamp: " + str(self.Timestamp))
+        print("CID: " + str(self.CID))
+        print("EID: " + str(self.EID))
+        print("State: " + str(self.State))
+        print("Data Length: " + str(self.DataLen))
+        print("Data: " + self.Data)
+        print("-------------------------------------------------------------")
+        
+# class static_block:
+#     #padding info (length in bytes) for state
+#     bytelen12 = 12
+#     #actual state assignment
+#     stateVar = b'\2'
+    
+#     PreviousHash = "26 19 13 b7 1a 13 30 6d 63 d6 56 12 87 5e ad 13 ed 28 1b 2f 70 f9 c7 13 76 12 47 f4 7a 9f f7 ee "
+    
+#     #function defined at the top, need to change to make 8 byte long float 
+#     Timestamp = getUnixTime()
+#     CID = 9269517611667619
+#     EID = 2179
+    
+#     #force padding of "a"s to the actual state assignment
+#     State = stateVar.rjust(bytelen12, b'\a')
+#     #length of following Data
+#     DataLen = 100
+#     #Data at length of 100
+#     Data = "uwF6MJKlWZ6G1JQQuqXRVHAwYIusJQBhhL7KFtrnxnN9xTwhESWbAi6MI09OfyGYUFbp8vUMEJcwCIJInReYggGt9HsS3QzjDc1h"
+        
+# print("Static Block contents: ")
+# print("Previous hash: " + static_block.PreviousHash)
+# print("Timestamp: " + str(static_block.Timestamp))
+# print("CID: " + str(static_block.CID))
+# print("EID: " + str(static_block.EID))
+# print("State: " + str(static_block.State))
+# print("Data Length: " + str(static_block.DataLen))
+# print("Data: " + static_block.Data)
+
+# b = Block()
+# b.setCID(9269517611667619)
+# b.setData("uwF6MJKlWZ6G1JQQuqXRVHAwYIusJQBhhL7KFtrnxnN9xTwhESWbAi6MI09OfyGYUFbp8vUMEJcwCIJInReYggGt9HsS3QzjDc1h")
+# b.setEID(2179)
+# b.setPreviousHash("261913b71a13306d63d65612875ead13ed281b2f70f9c713761247f47a9ff7ee")
+# b.setTimestamp()
+# b.setDataLength(100)
+# b.printBlock()
+
+# b.blockToBytes()
--- Data.py
+++ Data.py
@@ -1,4 +1,14 @@
 # TODO Johnny
+import Block
+import os
+import struct
+
+def tupHex(tuple):
+    newList = []
+    for i in tuple:
+        newList.append(hex(i)) 
+    return newList
+
 class Data:
     def __init__(self):
         self.CaseID = 0
@@ -7,3 +17,13 @@ class Data:
         # INITIAL (for the initial block ONLY), CHECKEDIN, CHECKEDOUT, DISPOSED, DESTROYED, or RELEASED
         # -1,                                   0,          2,          3,          4,          5
         #states need to be padded to 12 bytes
+    def importData(self, file):
+        filesize = os.path.getsize(file)
+        with open(file,'rb') as f:
+                block = f.read(filesize)
+                block = struct.unpack(str(filesize)+"B",block)
+        part = block[0:17]
+        part = tupHex(part)
+        print(part)
+d = Data()
+d.importData("2023-03-23T001804.594218.raw")
