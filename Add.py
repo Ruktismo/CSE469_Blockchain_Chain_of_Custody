f9cba0454ba7919fc1a9bd9ddc9f96efc8b57b0b
Zekaeria <anielle.david09@gmail.com>
2023-04-13 22:49:52 -0700

    byte checking & more updates

--- Add.py
+++ Add.py
@@ -5,36 +5,85 @@ verify if stuff isn't in use
 make blocks
 commit to chain error checking - check if bytes r correct length in
 """
-import Block
+
+import random
+import secrets
+import string
+import uuid
 from Block import Block
-from Data import Data
 from Block_Chain import BC
 
 
+# check if case_id is a valid uuid, works for strings w/ hyphens & no hyphens
+def isValidUuid(cid):
+    try:
+        uuid.UUID(str(cid))
+        return True
+    except ValueError:
+        return False
+
+# check if item_id is a valid 4 byte int
+def isValidInt(eid):
+    if eid.isdigit():
+        return True
+    else:
+        return False
+
+# remove dashes from uuid
+def uuidToHex(u: str) -> str:
+    return uuid.UUID(u).hex
+
+# generate random string based on given random data length
+def getRandomString(dataLength):
+    res = ''.join(secrets.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)
+                  for i in range(dataLength))
+    return str(res)
+
+
 def add(case_id, item_ids):
-    #print("\nCase: " + case_id)
+    # Check if CASE ID is UUID
+    if not isValidUuid(case_id):
+        print("Error: case_id is not a valid UUID. Must input valid UUID")
+        exit()
 
-    # 1. check if case_id is checked out
+    # Case ID Byte Check Passed, for ea item_id, check BC list if case id && item_id exists
+    print("\nCase: " + case_id)
+    for j in item_ids:
+        # check if item_id[j] is INT
+        if isValidInt(item_ids[j]):
+            # iter through blockchain. If item already exists, exit. Else add new block to chain
+            for i in BC.datalist:
+                if i.CaseID == case_id:
+                    if i.EvidenceID == item_ids[j]:
+                        print("Error: Cannot add an existing item. Must add a new item. ")
+                        exit()
+                    else:
+                        # item_id is unique/new, add new block
+                        b = Block()
+                        hash = BC.getLatestHash()
+                        b.setPreviousHash(hash)
+                        b.setTimestamp()
 
-    # 2. if not checked out, check if ea item_id is duplicate, if duplicate throw error
-    # for ea item_id
-    #if BC.dataExists(BC, newData):
-        #print("\nItem already exists for this case file")
+                        # remove hyphens from uuid, store as hexstr
+                        b.setCID(uuidToHex(
+                            case_id))  # ex: '65cc391d-6568-4dcc-a3f1-86a2f04140f3' stored as '65cc391d65684dcca3f186a2f04140f3'
+                        b.setEID(item_ids[j])
+                        b.setState("000CHECKEDIN")
 
-    #else:
-        # if case_id isn't in use, error checking
-        # for ea item_ids (i=0), create block and add to file
-        # b = Block()
-        # b.setCID(case_id)
-        # b.setData("")
-        # b.setEID(item_ids[i])
-        # b.setState("000CHECKEDIN")
-        # b.setPreviousHash("")
-        # b.setTimestamp()
-        # b.setDataLength(100)
+                        # set data to new block
+                        b.setDataLength(random.randint(0, 32))  # set rand length of data (range: [0,32])
+                        b.setData(getRandomString(b.getDataLength()))  # gen rand str from data length
 
-        # confirmation block is added
-        #print("\nAdded item: " + item_ids[i])  # Added item: 987654321
-        #print("\nStatus: CHECKED IN")  # reformat
-        #print("\nTime of action: ")  # Time of action: 2022-01-22T03:13:07.820445Z
-        pass
+                        # get & print block
+                        print(f'Added item: {b.getEID()}')
+                        print(f'\tStatus: {b.getState()}')
+                        print(f'\tTime of action: {b.getTimestamp()}')
+
+                        b.blockToBytes()
+                        BC.reload()
+
+        # 2. item_id[j] is NOT an INT
+        else:
+            print("Error: item_id is not an integer. Must input integer")
+
+# add -c 65cc391d65684dcca3f186a2f04140f3 -i 987654321
--- Remove.py
+++ Remove.py
@@ -1,2 +1,58 @@
+# TODO: Anielle
+import uuid
+from Block_Chain import BC
+from Block import Block
+
+
 def remove(item_id, reason, owner):
-    pass
+    # go through the list to check if item_id exists
+    for i in BC.datalist:
+        if i.EvidenceID == item_id:
+            if i.State == "CHECKEDOUT":
+                print("Error: Cannot remove a checked out item. Must check it in first")
+                exit()
+            elif i.State == "INITIAL":
+                print("Error: Cannot remove an initial block.")  # possible to remove initial block?
+                exit()
+            elif i.State == "DISPOSED" or i.State == "DESTROYED" or i.State == "RELEASED":
+                print("Error: Cannot remove an already disposed/destroyed/released item")
+                exit()
+            elif reason != "DISPOSED" or reason != "DESTROYED" or reason != "RELEASED":
+                print("Error: Not a valid reason. Must be 'DISPOSED', 'DESTROYED', or 'RELEASED")
+                exit()
+            else:
+                # Remove block
+                dataBlock = BC.blockExists(i)  # for setting data
+                b = Block()
+
+                hash = BC.getLatestHash()
+                b.setPreviousHash(hash)
+                b.setTimestamp()
+                b.setCID(i.CID)
+                b.setEID(item_id)
+
+                # state needs to be 12 char in length
+                if reason == "DISPOSED":
+                    b.setState("0000DISPOSED")
+                elif reason == "DESTROYED":
+                    b.setState("000DESTROYED")
+                else:
+                    b.setState("0000RELEASED")
+
+                # set data & data length from existing block's
+                b.setDataLength(int(dataBlock.getDataLength()))
+                b.setData(dataBlock.getData())
+
+                # if storing CID as string w/ no hyphens (see Add.py, line 69), insert hyphens again
+                # print(f'Case: {uuid.UUID(hex=b.getCID())}')
+                print(f'Case: {b.getCID()}')
+                print(f'Removed item: {b.getEID()}')
+                print(f'\tStatus: {b.getState()}')
+                if (b.getState() == "RELEASED"):
+                    print(f'\tOwner info: {owner}')
+                print(f'\tTime of action: {b.getTimestamp()}')
+
+                b.blockToBytes()
+                BC.reload()
+
+# remove -i 987654321 -y RELEASED -o "John Doe, 123 Cherry Ln, Pleasant, AZ 84848, 480-XXX-4321"
