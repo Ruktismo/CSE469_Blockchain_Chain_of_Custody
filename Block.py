be3a5aa6a0bbc57bebed385d362e5f6478812168
johnavarro1 <jjnavar5@asu.edu>
2023-04-19 16:55:58 -0700

    Update Block.py

--- Block.py
+++ Block.py
@@ -139,7 +139,7 @@ class Block:
     def initToBytes(self):
         packed1 = struct.pack("32s", self.getPreviousHash())
         packed2 = self.getDoubleTimestamp()
-        packed3 = struct.pack("16s", str(self.getCID()))
+        packed3 = struct.pack("16s", self.getCID())
         packed4 = struct.pack("I", self.getEID())
         packed5 = struct.pack("12s", self.getState().encode())
         packed6 = struct.pack("I", self.getDataLength())
