9ef71265d8cc03625b5f3bf109f2de5a56e2271c
Zekaeria <anielle.david09@gmail.com>
2023-04-20 12:54:16 -0700

    restruct

--- Block.py
+++ Block.py
@@ -115,7 +115,7 @@ class Block:
         
         packed1 = struct.pack("32s", self.getPreviousHash())
         packed2 = self.getDoubleTimestamp()
-        packed3 = struct.pack("16s", str(self.getCID()).encode())
+        packed3 = struct.pack("16s", str(str(self.getCID())).encode())
         packed4 = struct.pack("I", self.getEID())
         packed5 = struct.pack("12s", self.getState().encode())
         packed6 = struct.pack("I", self.getDataLength())
