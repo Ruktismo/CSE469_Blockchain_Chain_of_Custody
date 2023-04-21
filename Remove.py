4f14bd7c0c3a1a5d341f6d3ad81b0eb56b3de599
Zekaeria <anielle.david09@gmail.com>
2023-04-20 22:44:52 -0700

    unpacking CID

--- Remove.py
+++ Remove.py
@@ -15,18 +15,18 @@ def remove(item_id, reason, owner):
     #check if item_id exists
     # go through the list to check if item_id exists
     print("in Remove()")
-    for i in BC.blockList:
-        if i.EID == item_id:
-            print(i.EID)
+    for i in BC.datalist:
+        if i.EvidenceID == item_id:
+            print(i.EvidenceID)
             print(item_id)
             print(reason)
-            if i.State == "CHECKEDOUT":
+            if i.state == "CHECKEDOUT":
                 print("Error: Cannot remove a checked out item. Must check it in first")
                 exit(-1)
-            elif i.State == "INITIAL":
+            elif i.state == "INITIAL":
                 print("Error: Cannot remove an initial block.")  # possible to remove initial block?
                 exit(-1)
-            elif i.State == "DISPOSED" or i.State == "DESTROYED" or i.State == "RELEASED":
+            elif i.state == "DISPOSED" or i.state == "DESTROYED" or i.state == "RELEASED":
                 print("Error: Cannot remove an already disposed/destroyed/released item")
                 exit(-1)
             elif reason != "DISPOSED" and reason != "DESTROYED" and reason != "RELEASED":
@@ -35,13 +35,16 @@ def remove(item_id, reason, owner):
             else:
                 print("removing block!")
                 # Remove block
+
+                dataBlock = BC.blockExists(i)
                 b = Block()
 
                 hash = BC.getLatestHash()
                 b.setPreviousHash(hash)
                 b.setTimestamp()
-                print(i.CID)
-                b.setCID(i.getCID()) #convert bytes to 16byte string
+                print(dataBlock.getCID())
+                #b.setCID(str(i.getCID())) #convert bytes to 16byte string
+                b.setCID(dataBlock.getCID())
                 b.setEID(int(item_id))
 
                 # state needs to be 12 char in length
@@ -53,8 +56,8 @@ def remove(item_id, reason, owner):
                     b.setState("RELEASED")
 
                 # set data & data length from existing block's
-                b.setDataLength(i.DataLen)
-                b.setData(i.Data)
+                b.setDataLength(0)
+                b.setData('')
 
                 #n = b.getCID()  # get int CID
                 #UUIDstr = intToHex(n)  # convert int CID to UUID str (w dashes)
