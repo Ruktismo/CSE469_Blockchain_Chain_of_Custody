2d35eeff4cda371685a0128edd3003344b46909d
Zekaeria <anielle.david09@gmail.com>
2023-04-21 11:22:34 -0700

    take in owner info args

--- Main.py
+++ Main.py
@@ -8,11 +8,12 @@ from Remove import remove
 from Init_Chain import init_chain
 from Verify_Chain import verify_chain
 
-
 """
 Main will deal with all setup and arg parsing.
 Once it has the cmd and all args needed it will pass off to the corresponding function.
 """
+
+
 def main():
     # TODO put any setup that needs to be done here. ie. load blockchain from disk
     # TODO Johnny
@@ -40,7 +41,7 @@ def main():
         item_ids = []
         for i in range(4, len(sys.argv)):
             if sys.argv[i] == "-i":
-                item_ids.append(sys.argv[i+1])
+                item_ids.append(sys.argv[i + 1])
         add(case_id, item_ids)
 
     elif sys.argv[1] == "checkout":
@@ -64,25 +65,25 @@ def main():
             if '-r' in sys.argv[i]:
                 is_reversed = True
             if '-n' in sys.argv[i]:
-                num_entries = int(sys.argv[i+1])
+                num_entries = int(sys.argv[i + 1])
             if '-c' in sys.argv[i]:
-                case_id = sys.argv[i+1]
+                case_id = sys.argv[i + 1]
             if '-i' in sys.argv[i]:
-                item_id = sys.argv[i+1]
+                item_id = sys.argv[i + 1]
         log(is_reversed, num_entries, case_id, item_id)
 
     elif sys.argv[1] == "remove":
         if len(sys.argv) == 6:
             remove(sys.argv[3], sys.argv[5], None)
-        elif sys.argv[6] == "-o":
+        elif len(sys.argv) >= 8 and sys.argv[6] == "-o":
             o_info = []
             for i in range(6, len(sys.argv)):
-                    o_info.append(sys.argv[i + 1])
+                o_info.append(sys.argv[i + 1])
             owner = ' '.join(str(o) for o in o_info)
             remove(sys.argv[3], sys.argv[5], owner)
-        #elif len(sys.argv) == 8:
-            #owner=None
-            #remove(sys.argv[3], sys.argv[5], owner)
+        # elif len(sys.argv) == 8:
+        # owner=None
+        # remove(sys.argv[3], sys.argv[5], owner)
         else:
             print("ERROR: Incorrect number of args\n Proprer format:  bchoc remove -i item_id -y reason [-o owner]")
             exit(-1)
